adbf00679b4f92d977f3c965ab24719f1af28c0e
Dimitri <dacost4@uic.edu>
2014-06-20 17:56:40 -0500

    R code for assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## this function will ONLY cache square matrices ie. 3x3
+makeCacheMatrix <- function(x) {
+  m <- NULL ##placeholder for cached solved matrix
+  q<-dim(x) ##used to test matrix
+  bad<-"BZZZ! No dice, your matrix doesn't make sense"
+  if(q[1]!=sqrt(length(x))){
+    return(bad)
+    ##determines if matrix is square,
+    ##returns error if the matrix is NOT square
+  }
+  if(det(x)==0){
+    return(bad)
+    ##determines if inverse is possible, 
+    ##returns error if the matrix is singular
+  }
+  get <- function() x             ##caches value of original matrix 'x'
+  slvm <- function(slv) m <<- slv ##caches inverse of matix 'x'
+  getm <- function() m            ##caches computation of inverse
+  list(get = get,
+       slvm = slvm,
+       getm = getm)
+  ##list order of functions in the global environment
 }
 
-
-## Write a short comment describing this function
-
+##this function returns the inverse matrix of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getm()     ##pulls past computation of inverse and sets to 'm'
+  if(!is.null(m)) { ##test if 'm' is NOT empty
+    message("getting cached data")
+    return(m)       ##prints 'm' if NOT empty
+  }
+  data <- x$get()   ##pulls original matrix and sets to 'data'
+  m <- solve(data)  ##solves for matrix inverse and sets to 'm'
+  x$slvm(m)         ##sets solved inverse to 'slvm'
+  m                 ##prints 'm'
 }
